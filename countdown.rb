1941cf12eb563e672d13b3f08b909a0969a0153c
Laura Berge <lauraeberge@gmail.com>
2019-09-09 15:18:54 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,22 @@
 #write your code here
 
-def countdown
+def countdown(n)
+  until n == 0 do
+    puts "#{n} SECOND(S)!"
+    n -= 1
+  end
+  if n == 0
+    return "HAPPY NEW YEAR!"
+  end
+end
+
+def countdown_with_sleep(n)
+    until n == 0 do
+    puts "#{n} SECOND(S)!"
+    n -= 1
+    sleep 1
+  end
+  if n == 0
+    return "HAPPY NEW YEAR!"
+  end
 end

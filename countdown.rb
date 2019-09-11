86f11e550cf34e216cb169ec0b480b7a9fa056dc
Srikalyan Megati <kalmegati@gmail.com>
2019-09-11 19:51:20 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,24 @@
 #write your code here
 
-def countdown
+def countdown(tMinus)
+  
+  while tMinus > 0
+    puts "#{tMinus} SECOND(S)!"
+    tMinus -= 1 
+  end
+  
+  "HAPPY NEW YEAR!"
+  
+end
+
+def countdown_with_sleep(tMinus)
+  
+  while tMinus > 0
+    puts "#{tMinus} SECOND(S)!"
+    sleep(1)
+    tMinus -= 1 
+  end
+  
+  "HAPPY NEW YEAR!"
+  
 end

9501808d3100272fd21ec6bbc98bd5a7b3d2d2c5
Jalysa Reason <jalysareason@gmail.com>
2019-09-13 17:30:37 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,12 @@
-#write your code here
-
 def countdown
+  X=10
+  puts countdown"#{10} SECOND(S)!"
+  X-= 10
 end
+
+HAPPY NEW YEAR!
+
+def countdown_with_sleep
+    puts countdown_with_sleep("should take at least 5 seconds to execute")
+      runtime = Benchmark.measure { countdown_with_sleep(10) }
+    end

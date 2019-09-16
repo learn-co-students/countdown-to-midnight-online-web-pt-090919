e46cbc48d1cb45d5080967cf3e627147097132f6
Ashley Twomey <aprier091012@gmail.com>
2019-09-16 03:45:36 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,12 +1,19 @@
-def countdown(10)
-  
-  new_year = 10
-  while new_year < 10
-  puts string
- new_year += 1
- end
+def countdown(time_to_midnight)
+  while time_to_midnight > 0
+    puts "#{time_to_midnight} SECOND(S)!"
+    time_to_midnight -= 1
+  end
+  time_to_midnight.to_s + " left. HAPPY NEW YEAR!"
+  "HAPPY NEW YEAR!"
 end
   
-  
+  def countdown_with_sleep(time_to_midnight)
+  while time_to_midnight > 0
+    puts "#{time_to_midnight} SECOND(S)!"
+    sleep(1)
+    time_to_midnight -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
   
 

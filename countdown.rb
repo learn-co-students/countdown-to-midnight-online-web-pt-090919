036e72580aeb2f7a160c791d48129f60fbb44a3a
Ally Morson <ally.morson@gmail.com>
2019-09-19 03:16:52 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,7 +1,7 @@
-def countdown(10)
+def countdown
 number = 10 
 while number >= 0
-puts "#{number} SECONDS"
+puts "#{number} SECOND(S)"
 number = number - 1
 end 
 puts "HAPPY NEW YEAR"
@@ -11,10 +11,27 @@ countdown
 def countdown_with_sleep
 number = 10 
 while number >= 0
-puts "#{number} SECONDS"
+puts "#{number}"
 number = number - 1
-sleep (1) 
+sleep (0.5) 
 end
 puts "HAPPY NEW YEAR"
 end 
 countdown_with_sleep
+
+def countdown(seconds_to_midnight)
+   while seconds_to_midnight > 0
+     puts "#{seconds_to_midnight} SECOND(S)!"
+     seconds_to_midnight -= 1
+   end
+   "HAPPY NEW YEAR!"
+ end
+ 
+def countdown_with_sleep(seconds_to_midnight)
+   while seconds_to_midnight > 0
+     puts "#{seconds_to_midnight} SECOND(S)!"
+     sleep(1)
+     seconds_to_midnight -= 1
+   end
+   "HAPPY NEW YEAR!"
+ end
